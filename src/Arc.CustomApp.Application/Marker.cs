7fdb9903d35f54f51cda8c6ca27a248115a1686f
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-01 08:19:48 +0530

    wip

--- src/Arc.CustomApp.Application/Marker.cs
+++ src/Arc.CustomApp.Application/Marker.cs
@@ -1,6 +1,4 @@
 ﻿namespace Arc.CustomApp.Application;
 
-/// <summary>
-/// Represents a marker used to identify mediator handlers in the DI.
-/// </summary>
+/// <summary>A marker used to identify mediator handlers in the DI.</summary>
 public class Marker;
