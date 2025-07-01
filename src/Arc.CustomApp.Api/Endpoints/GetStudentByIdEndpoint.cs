ba6c8a81565cd9d3f6263e5978f4a61f50adf888
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-01 08:23:38 +0530

    wip

--- src/Arc.CustomApp.Api/Endpoints/GetStudentByIdEndpoint.cs
+++ src/Arc.CustomApp.Api/Endpoints/GetStudentByIdEndpoint.cs
@@ -1,20 +1,9 @@
 ﻿namespace Arc.CustomApp.Api.Endpoints;
 
-/// <summary>
-/// Provides functionality to map the "get-student-by-id" endpoint, which retrieves student details by their unique
-/// identifier.
-/// </summary>
-/// <remarks>This endpoint is designed to handle HTTP GET requests to the "get-student-by-id" route. It processes
-/// the request by querying for a student based on the provided ID and returns either the student's details or an error
-/// if the student is not found.</remarks>
 internal static class GetStudentByIdEndpoint
 {
-    /// <summary>
-    /// Maps an HTTP GET endpoint to retrieve a student by their unique identifier.
-    /// </summary>
-    /// <remarks>The endpoint is registered at the route "get-student-by-id". It expects a query parameter 
-    /// specifying the student's ID.</remarks>
-    /// <param name="builder">The <see cref="IEndpointRouteBuilder"/> used to configure the endpoint.</param>
+    /// <summary>Maps the "get-student-by-id" endpoint to retrieve student data.</summary>
+    /// <param name="builder">The endpoint builder to configure the endpoint.</param>
     public static void MapGetStudentByIdEndpoint(this IEndpointRouteBuilder builder) =>
         builder.MapGet("get-student-by-id", Handle);
 
