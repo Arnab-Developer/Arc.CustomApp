a33f86a9f62008e0dbdae34f0d04d816d3d67684
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-02 10:46:26 +0530

    create validation test

---
+++ test/Arc.CustomApp.ApplicationTest/Behaviors/ValidationBehaviorTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Arc.CustomApp.ApplicationTest.Behaviors;
+
+public partial class ValidationBehaviorTest
+{
+    private readonly Mock<IValidator<IRequest<bool>>> _validatorMock = new();
+    private readonly IEnumerable<IValidator<IRequest<bool>>> _validators;
+    private readonly ValidationBehavior<IRequest<bool>, bool> _validationBehavior;
+
+    public ValidationBehaviorTest()
+    {
+        _validators = [_validatorMock.Object];
+        _validationBehavior = new(_validators);
+    }
+}
