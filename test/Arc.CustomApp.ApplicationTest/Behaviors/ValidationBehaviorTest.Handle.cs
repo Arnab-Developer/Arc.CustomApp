086c99fd6b4864b3a28775e69c9235c67a7c7aee
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-02 11:52:08 +0530

    success test

--- test/Arc.CustomApp.ApplicationTest/Behaviors/ValidationBehaviorTest.Handle.cs
+++ test/Arc.CustomApp.ApplicationTest/Behaviors/ValidationBehaviorTest.Handle.cs
@@ -1,6 +1,28 @@
-﻿namespace Arc.CustomApp.ApplicationTest.Behaviors;
+﻿using FluentValidation.Results;
+
+namespace Arc.CustomApp.ApplicationTest.Behaviors;
 
 public partial class ValidationBehaviorTest
 {
+    [Fact]
+    public async Task Handle_DoNotFail_GivenValidId()
+    {
+        // Arrange
+        _validatorMock
+            .Setup(m => m.ValidateAsync(_requestMock.Object, _cancellationToken))
+            .ReturnsAsync(new ValidationResult());
 
+        // Act
+        var response = await _validationBehavior.Handle(
+            _requestMock.Object, _nextMock.Object, _cancellationToken);
+
+        // Assert
+        _validatorMock.Verify(m => m.ValidateAsync(_requestMock.Object, _cancellationToken), 
+            Times.Once());
+
+        _validatorMock.VerifyNoOtherCalls();
+
+        _nextMock.Verify(m => m(_cancellationToken), Times.Once());
+        _nextMock.VerifyNoOtherCalls();
+    }
 }
--- test/Arc.CustomApp.ApplicationTest/Behaviors/ValidationBehaviorTest.cs
+++ test/Arc.CustomApp.ApplicationTest/Behaviors/ValidationBehaviorTest.cs
@@ -5,9 +5,12 @@ namespace Arc.CustomApp.ApplicationTest.Behaviors;
 
 public partial class ValidationBehaviorTest
 {
+    private readonly Mock<IRequest<bool>> _requestMock = new();
     private readonly Mock<IValidator<IRequest<bool>>> _validatorMock = new();
     private readonly IEnumerable<IValidator<IRequest<bool>>> _validators;
     private readonly ValidationBehavior<IRequest<bool>, bool> _validationBehavior;
+    private readonly CancellationToken _cancellationToken = CancellationToken.None;
+    private readonly Mock<RequestHandlerDelegate<bool>> _nextMock = new();
 
     public ValidationBehaviorTest()
     {
