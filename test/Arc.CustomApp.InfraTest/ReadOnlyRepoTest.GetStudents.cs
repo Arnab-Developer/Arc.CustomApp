a5d6089e5c6cf5627ca81d54e8a20189425198b2
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-05 16:39:45 +0530

    Fix formatting

--- test/Arc.CustomApp.InfraTest/ReadOnlyRepoTest.GetStudents.cs
+++ test/Arc.CustomApp.InfraTest/ReadOnlyRepoTest.GetStudents.cs
@@ -7,12 +7,12 @@ public partial class ReadOnlyRepoTest
     {
         // Arrange
         var expectedStudents = new List<Student>()
-       {
+        {
             new(1, "S1", "Sub1"),
             new(2, "S2", "Sub2"),
             new(3, "S3", "Sub3"),
             new(4, "S4", "Sub4")
-       };
+        };
 
         // Act
         var actualStudents = await _readOnlyRepo.GetStudents(_cancellationToken);
